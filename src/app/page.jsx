2f28c41744e23b9cbb0af76363eae54fa483dc08
mary1na-code <zioladesino@gmail.com>
2026-06-05 23:48:58 -0700

    feat: add global design components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,11 +5,11 @@ import Link from "next/link"
 export default function Home() {
   return (
     <main>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the main content of the home page.</p>
-      <p>Autosave is on!</p>
-      <p>hello, world!</p>
-      <Link href="/users">Users</Link>
+      <h1>Welcome to Study Buddy</h1>
+      <p>Get to studying!</p>
+      <p>Study Buddy helps you study better with quizzes, flashcards and an ai study assistant. Go to dashboard to get started now!</p>
+      <Link href="/dashboard">Go to Dashboard</Link>
     </main>
   );
 }
+ 
